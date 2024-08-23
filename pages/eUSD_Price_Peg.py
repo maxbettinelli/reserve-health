ff9e37d8d4ed4e90e853771f31de6ab7ee7407db
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2024-08-23 07:49:55 -0700

    requirements modification

--- pages/eUSD_Price_Peg.py
+++ pages/eUSD_Price_Peg.py
@@ -46,8 +46,8 @@ st.markdown("---")
 st.sidebar.markdown("# eUSD Price Peg")
 st.sidebar.markdown("---")
 
-# Path to your CSV file
-csv_path = 'https://raw.githubusercontent.com/maxbettinelli/reserve-health/master/aug22_data/eusd_pricepeg.csv'
+# # Path to your CSV file
+# csv_path = 'https://raw.githubusercontent.com/maxbettinelli/reserve-health/master/aug22_data/eusd_pricepeg.csv'
 
 # Load the CSV file into a DataFrame
 eusd_price_peg_aum_query_result = pd.read_csv(csv_path)
